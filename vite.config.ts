755cdd7460c44d93e13f816226564c4d0e448c55
abdelmac <149905816+abdelmac@users.noreply.github.com>
2025-06-22 20:51:54 +0200

    er

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/',
+  // or './' if your custom domain is not working yet
 
 // https://vitejs.dev/config/
 export default defineConfig({
