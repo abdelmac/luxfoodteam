7a954153a5616aef0b03be593d9a5f2a5016b125
abdelmac <149905816+abdelmac@users.noreply.github.com>
2025-06-25 22:16:42 +0200

    test

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -159,7 +159,7 @@ export default function Products() {
                   })}
                   </p>
                 </div>
-              </div>
+              </div> 
             </Link>
           ))}
         </div>
